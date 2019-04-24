58d93e31fbf99e96f6d3a23a209db591cb7b2fd7
zyq-Tomato <1562070422@qq.com>
2019-04-24 15:10:27 +0800

    webpack配置/prod.conf注释

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,40 +5,53 @@ var config = require('../config')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+// copy-webpack-plugin，用于将static中的静态文件复制到产品文件夹dist
 var CopyWebpackPlugin = require('copy-webpack-plugin')
 // var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+// optimize-css-assets-webpack-plugin，用于优化和最小化css资源
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var MpvueVendorPlugin = require('webpack-mpvue-vendor-plugin')
+
 var env = config.build.env
 
 var webpackConfig = merge(baseWebpackConfig, {
   module: {
+     // 样式文件的处理规则，对css/sass/scss等不同内容使用相应的styleLoaders
+    // 由utils配置出各种类型的预处理语言所需要使用的loader，例如sass需要使用sass-loader
     rules: utils.styleLoaders({
       sourceMap: config.build.productionSourceMap,
       extract: true
     })
   },
+  // 是否使用source-map
   devtool: config.build.productionSourceMap ? '#source-map' : false,
+  // webpack输出路径和命名规则
   output: {
     path: config.build.assetsRoot,
-    // filename: utils.assetsPath('[name].[chunkhash].js'),
-    // chunkFilename: utils.assetsPath('[id].[chunkhash].js')
-    filename: utils.assetsPath('[name].js'),
-    chunkFilename: utils.assetsPath('[id].js')
+    // filename: utils.assetsPath('js/[name].[chunkhash].js'),
+    // chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name].js'),
+    chunkFilename: utils.assetsPath('js/[id].js')
   },
+  // webpack插件
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    // extract css into its own file
+     // 丑化压缩JS代码
+    new UglifyJsPlugin({
+      sourceMap: true
+    }),
+    // 将css提取到单独的文件
     new ExtractTextPlugin({
-      // filename: utils.assetsPath('[name].[contenthash].css')
-      filename: utils.assetsPath(`[name].${config.build.fileExt.style}`)
+      // filename: utils.assetsPath('css/[name].[contenthash].css')
+      filename: utils.assetsPath('css/[name].wxss')
     }),
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
+    // 优化、最小化css代码，如果只简单使用extract-text-plugin可能会造成css重复
+    // 具体原因可以看npm上面optimize-css-assets-webpack-plugin的介绍
     new OptimizeCSSPlugin({
       cssProcessorOptions: {
         safe: true
@@ -47,25 +60,28 @@ var webpackConfig = merge(baseWebpackConfig, {
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
+    // 将产品文件的引用注入到index.html
     // new HtmlWebpackPlugin({
     //   filename: config.build.index,
     //   template: 'index.html',
     //   inject: true,
     //   minify: {
-    //     removeComments: true,
-    //     collapseWhitespace: true,
-    //     removeAttributeQuotes: true
+    //     removeComments: true,  // 删除index.html中的注释
+    //     collapseWhitespace: true,  // 删除index.html中的空格
+    //     removeAttributeQuotes: true  // 删除各种html标签属性值的双引号
     //     // more options:
     //     // https://github.com/kangax/html-minifier#options-quick-reference
     //   },
     //   // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+         // 注入依赖的时候按照依赖先后顺序进行注入，比如，需要先注入vendor.js，再注入app.js  
     //   chunksSortMode: 'dependency'
     // }),
     // keep module.id stable when vender modules does not change
     new webpack.HashedModuleIdsPlugin(),
     // split vendor js into its own file
+    // 将所有从node_modules中引入的js提取到vendor.js，即抽取库文件
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'common/vendor',
+      name: 'vendor',
       minChunks: function (module, count) {
         // any required modules inside node_modules are extracted to vendor
         return (
@@ -77,16 +93,23 @@ var webpackConfig = merge(baseWebpackConfig, {
     }),
     // extract webpack runtime and module manifest to its own file in order to
     // prevent vendor hash from being updated whenever app bundle is updated
+     // 从vendor中提取出manifest，原因如上
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'common/manifest',
-      chunks: ['common/vendor']
+      name: 'manifest',
+      chunks: ['vendor']
     }),
-    new MpvueVendorPlugin({
-      platform: process.env.PLATFORM
-    })
+    // copy custom static assets
+    // 将static文件夹里面的静态资源复制到dist/static
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static'),
+        to: config.build.assetsSubDirectory,
+        ignore: ['.*']
+      }
+    ])
   ]
 })
-
+// 如果开启了产品gzip压缩，则利用插件将构建后的产品文件进行压缩
 // if (config.build.productionGzip) {
 //   var CompressionWebpackPlugin = require('compression-webpack-plugin')
 
@@ -104,17 +127,10 @@ var webpackConfig = merge(baseWebpackConfig, {
 //     })
 //   )
 // }
-
+// 如果启动了report，则通过插件给出webpack构建打包后的产品文件分析报告
 if (config.build.bundleAnalyzerReport) {
   var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
   webpackConfig.plugins.push(new BundleAnalyzerPlugin())
-}
-
-var useUglifyJs = process.env.PLATFORM !== 'swan'
-if (useUglifyJs) {
-  webpackConfig.plugins.push(new UglifyJsPlugin({
-    sourceMap: true
-  }))
 }
 
 module.exports = webpackConfig
